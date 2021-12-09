527399c626f5cc3a02aece233f10ecc9d2981660
Konstantin Zhiliaev <k926227@gmail.com>
2021-12-09 08:09:12 +0300

    refactor: change variables names

--- src/Components/PokemonNamesList.tsx
+++ src/Components/PokemonNamesList.tsx
@@ -6,15 +6,14 @@ import { NamesList } from './NamesList';
 
 export const PokemonNamesList = (): JSX.Element => {
   const [urlToFetch, setUrlToFetch] = React.useState(`${settings.restApi}/pokemon`);
-  const prevNames = React.useRef<string[]>([]);
+  const pokemonNames = React.useRef<string[]>([]);
   let { data, error } = useSWR<IAllPokemonApiInfo>(urlToFetch, get);
 
   if (error) return <div>Error!</div>;
-  // if (!data) return <div>Loading...</div>;
 
-  //To prevent page blinks
-  let pokemonNames = data?.results.map((info) => info.name) || prevNames.current;
-  prevNames.current = pokemonNames;
+  if (data) {
+    pokemonNames.current = data.results.map((info) => info.name);
+  }
 
   const onPressPrev = (): void => {
     if (data?.previous) {
@@ -31,7 +30,7 @@ export const PokemonNamesList = (): JSX.Element => {
   return (
     <>
       <h1>Pokemon names list</h1>
-      <NamesList names={pokemonNames} />
+      <NamesList names={pokemonNames.current} />
       <div>
         <button onClick={onPressPrev} disabled={!data?.previous}>
           Prev
